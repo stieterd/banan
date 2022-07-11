55e0ccdc1657c35936051c6de4ac577456ad4888
stieterd <PeViHo@outlook.com>
2022-07-11 23:53:28 +0200

    banaan

--- dllmain.cpp
+++ dllmain.cpp
@@ -25,21 +25,65 @@ void hookedSendPos(void* player) {
     // Only get localplayer
     if (((bool(__cdecl*)(void*))((uintptr_t)moduleBase + (uintptr_t)0x36BDB0))(player)){
 
-        printf("Localplayer: %p\n", player);
-        int maxHealth = *(int*)((uintptr_t)player + (uintptr_t)0x40);
-        printf("Maxhealth: %d\n", maxHealth);
+        //printf("Localplayer: %p\n", player);
 
-        Vec3 pos = *(Vec3*)((uintptr_t)player + (uintptr_t)0x80);
+        void* rigidbody = *(void**)((uintptr_t)player + (uintptr_t)0x70);
+        if (rigidbody) {
+            Vec3 position = ((Vec3(__cdecl*)(void*))((uintptr_t)moduleBase + (uintptr_t)0x1AB3EC0))(rigidbody);
+            printf("Position: %f,%f,%f\n", position.x, position.y, position.z);
+            position.y = 10;
+            ((void(__cdecl*)(void*, Vec3))((uintptr_t)moduleBase + (uintptr_t)0x1AB4140))(rigidbody, position);
+            
+        }
+
+
+        int maxHealth = *(int*)((uintptr_t)player + (uintptr_t)0x40);
+        //printf("Maxhealth: %d\n", maxHealth);
 
         // Get kills
         short kills = ((short(__cdecl*)(void*))((uintptr_t)moduleBase + (uintptr_t)0x36BDD0))(player);
-        printf("kills: %hd\n", kills);
+        //printf("kills: %hd\n", kills);
+
+        // void* aimtarget = *(void**)((uintptr_t)player + (uintptr_t)0x138);
+        // printf("aimtarget: %p\n", aimtarget);
+
+        // Vec3 pos = *(Vec3*)((uintptr_t)player + (uintptr_t)0x80);
+        // printf("position: %f,%f,%f\n", pos.x, pos.y, pos.z);
+        
 
         // take health
         //((void(__cdecl*)(void*, int))((uintptr_t)moduleBase + (uintptr_t)0x3692C0))(player, 100);
         
         // Set dead method
-        ((void(__cdecl*)(void*, bool))((uintptr_t)moduleBase + (uintptr_t)0x36BE20))(player, true);
+        // ((void(__cdecl*)(void*, bool))((uintptr_t)moduleBase + (uintptr_t)0x36BE20))(player, true);
+
+        void* playermovement = *(void**)((uintptr_t)player + (uintptr_t)0x18);
+
+        // printf("playermovementptr: %p\n", playermovement);
+        Vec3 velocity = ((Vec3(__thiscall*)(void*))((uintptr_t)moduleBase + (uintptr_t)0x38FF70))(playermovement);
+        //printf("vel: (%f,%f,%f)\n", velocity.x,velocity.y,velocity.z);
+
+        void* righthandtarget = *(void**)((uintptr_t)player + (uintptr_t)0x138);
+        if (righthandtarget)printf("target: %p\n", righthandtarget);
+
+        if (GetAsyncKeyState(VK_NUMPAD5) & 1) {
+        // if (true){
+            Vec3 spawnPos = { 20,15,20 };
+            ((void(__cdecl*)(void*, Vec3))((uintptr_t)moduleBase + (uintptr_t)0x361D90))(player, spawnPos);
+        }
+        void* multiplayerweapon = *(void**)((uintptr_t)player + (uintptr_t)0xA8);
+        //printf("multiplayerweapon: %p\n", multiplayerweapon);
+        if (multiplayerweapon != nullptr) {
+            
+            // Vec3 recoilOffset = *(Vec3*)((uintptr_t)multiplayerweapon + (uintptr_t)0xB4);
+            // printf("current weapon recoiloffset: (%f,%f,%f)\n", recoilOffset.x, recoilOffset.y, recoilOffset.z);
+            //float reloadtime = *(float*)((uintptr_t)multiplayerweapon + (uintptr_t)0xEC);
+            //printf("reloadtime: %f", reloadtime);
+
+            //((void(__cdecl*)(void*))((uintptr_t)moduleBase + (uintptr_t)0x3AC330))(multiplayerweapon);
+        
+        }
+
     }
     else {
         // Set dead method
